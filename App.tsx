f95bb9976f70151d1a4ac6b4b93a9bd67a454a31
Thiago <thiagoF89.tf@gmail.com>
2021-03-03 21:51:40 -0300

    :tada: started :tada:

--- App.tsx
+++ App.tsx
@@ -1,12 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Alert } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+
 
 export default function App() {
+
+  let userDefault = 'admin'
+  let passwordDefault = '1234'
+
+  const [userSubmit, setuUserSubmit] = useState('')
+  const [passwordSubmit, setPasswordSubmit] = useState('')
+
+  function clickedButton() {
+    if (userDefault === userSubmit && passwordDefault === passwordSubmit) {
+      Alert.alert(
+        ":)",
+        "Login feito com sucesso!"
+      )
+    } else {
+      Alert.alert(
+        ";-;",
+        "Ops, usuário incorreto!"
+      )
+    }
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.boxForm}>
+
+        <Text style={styles.title}>LOGIN</Text>
+
+        <Text style={styles.label}>Usuário:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={ text => {setuUserSubmit(text)}}
+        />
+
+        <Text style={styles.label}>Senha:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={ text => {setPasswordSubmit(text)}}
+        />
+
+        <RectButton onPress={clickedButton} style={styles.button}>
+          <Text style={styles.textButton}>Prosseguir</Text>
+        </RectButton>
+
+      </View>
     </View>
   );
 }
@@ -16,6 +60,45 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  boxForm: {
+    alignItems: 'center'
+  },
+
+  title: {
+    fontSize: 50,
+    marginTop: 200,
+    marginBottom: 50
+  },
+
+  label: {
+    fontSize: 28,
+    marginBottom: 8
+  },
+
+  input: {
+    width: 200,
+    backgroundColor: '#555',
+    fontSize: 24,
+    marginBottom: 40,
+    textAlign: 'center',
+    borderRadius: 8,
+    padding: 2
+  },
+
+  button: {
+    backgroundColor: '#8447FF',
+    color: '#fff',
+    width: 150,
+    alignItems: 'center',
+    padding: 4,
+    justifyContent: 'center',
+    borderRadius: 8
+  },
+
+  textButton: {
+    fontSize: 22,
+    fontWeight: 'bold'
+  }
 });
